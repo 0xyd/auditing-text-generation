fd03b42e63a40adb2e7271e82a4322266b6ac8ee
0xyd <davisfreeman1015@gmail.com>
2019-10-27 19:29:30 +0100

    modified:   data_loader/load_sated.py Decode bytes to unicode.

--- data_loader/load_sated.py
+++ data_loader/load_sated.py
@@ -26,7 +26,7 @@ def load_users(p=SATED_TRAIN_USER):
     users = []
     with open(p, 'rb') as f:
         for line in f:
-            users.append(str(line).replace('\n', ''))
+            users.append(line.decode('ascii').replace('\n', ''))
     return users
 
 
@@ -34,8 +34,7 @@ def load_texts(p=SATED_TRAIN_ENG):
     texts = []
     with open(p, 'rb') as f:
         for line in f:
-            line = str(line)
-            arr = ['<sos>'] + line.replace('\n', '').split(' ') + ['<eos>']
+            arr = ['<sos>'] + line.decode('ascii').replace('\n', '').split(' ') + ['<eos>']
             words = []
             for w in arr:
                 words.append(w)
@@ -197,8 +196,7 @@ def read_europarl_file(filename, num_lines=80000):
     texts = []
     with open(filename, 'rb') as f:
         for line in f:
-            line = line.lower()
-            arr = ['<sos>'] + line.lower().replace('\n', '').split(' ') + ['<eos>']
+            arr = ['<sos>'] + line.decode('ascii').lower().replace('\n', '').split(' ') + ['<eos>']
             texts.append(arr)
             if len(texts) > num_lines:
                 break
