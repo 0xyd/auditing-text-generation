9f111ae32908f9a4ec21eff0e7bf816b8ac26ff1
0xyd <davisfreeman1015@gmail.com>
2019-10-27 10:42:50 +0100

    modified:   reddit_lm.py 	change import path of the helper

--- reddit_lm.py
+++ reddit_lm.py
@@ -8,9 +8,10 @@ from keras.optimizers import Adam
 
 from .data_loader.load_reddit import read_top_user_comments, read_test_comments
 from .data_loader.load_wiki import load_wiki_by_users, load_wiki_test_data
+from .helper import DenseTransposeTied, flatten_data, iterate_minibatches, words_to_indices
 # from data_loader.load_reddit import read_top_user_comments, read_test_comments
 # from data_loader.load_wiki import load_wiki_by_users, load_wiki_test_data
-from helper import DenseTransposeTied, flatten_data, iterate_minibatches, words_to_indices
+# from helper import DenseTransposeTied, flatten_data, iterate_minibatches, words_to_indices
 
 
 MODEL_PATH = './data/reddit/model/'
